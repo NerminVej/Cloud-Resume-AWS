a57b89679205bb8cd800bd1736fcf82212547620
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-01 08:54:05 +0200

    added updateCounter function

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const counter = document.querySelector(".counter-num");
+async function updateCounter() {
+    let response = await fetch("https://vn5uuvvlw6i4yjpgk5x5pg45oe0qhlis.lambda-url.eu-central-1.on.aws/")
+    let data = await response.json();
+    counter.innerHTML = ` Views: ${data}`;
+}
+
+updateCounter();
